a07f58e0c5617310f7ae95612386eeeccdfdddbc
Sager Alarifi <sagr.1993@gmail.com>
2020-02-19 09:53:11 +0300

    Done with the solution

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import Cards from './Cards'
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    question: '',
+    cards: []
+  }
+}
+
+
+  handleChange = (event) =>{
+    console.log(event.target.value)
+    this.setState({ question: event.target.value })
+
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const question = this.state.question
+    const card = this.state.cards
+    card.push({ question })
+    console.log(card);
+
+    this.setState({
+      cards: card,
+      question: ''
+    })
+  }
+
+  render() {
+    return (
+      <section id="add-card" className="container">
+        <div className="row">
+          <h1>What's your question?</h1>
+          <form name="add-card">
+            <input
+              type="text"
+              className="question"
+              name="question"
+              placeholder="Type a question here!"
+              value={this.state.question}
+              onChange={this.handleChange}
+               />
+            <input type="submit" className="btn-submit" value="Submit" onClick={this.handleSubmit} />
+          </form>
+        </div>
+        <Cards cards={this.state.cards} />        
+      </section>
+    )
+  }
+}
+
+export default App;
